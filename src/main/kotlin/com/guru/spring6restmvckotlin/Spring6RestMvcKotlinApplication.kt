874899b501985bf13d2ceed83c3fe62dcfa13814
a.karamiani <aziz.karamiani@gmail.com>
2023-06-01 10:42:06 +0330

    Initial Commit

---
+++ src/main/kotlin/com/guru/spring6restmvckotlin/Spring6RestMvcKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.guru.spring6restmvckotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring6RestMvcKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<Spring6RestMvcKotlinApplication>(*args)
+}
